8e6de1c3b9a408ca294a8d055487fd3d2fe47fbd
Luckyphannavongsa <lukky7879@gmail.com>
2024-01-03 22:37:57 +0700

    first commit

---
+++ lib/list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:lucky_2cw3_homeworkc_ch9/header.dart';
+import 'package:lucky_2cw3_homeworkc_ch9/row.dart';
+import 'package:lucky_2cw3_homeworkc_ch9/row_card.dart';
+
+class list_view extends StatelessWidget {
+  const list_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chapter 9 list view"),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (BuildContext context, index) {
+            if (index == 0) {
+              return header(index: index);
+            } else if (index >= 1 && index <= 3) {
+              return rowcard(index: index);
+            } else {
+              return rower(
+                index: index,
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
